850e223e0f3619554acc559a9e08f5e788544cf5
OskarMolander <oskar.molander@lund.ntig.se>
2017-09-13 22:04:37 +0200

    Player now start with 3 boosts. Boosts regenerate every 3 seconds.

--- Space Scavenger/Boost.cs
+++ Space Scavenger/Boost.cs
@@ -10,8 +10,9 @@ namespace Space_Scavenger
 {
     public class Boost : DrawableGameComponent
     {
-        public int BoostTime = 0;
-        public int NrOfBoosts = 0;
+        public int BoostTime;
+        public int NrOfBoosts;
+        public int BoostRegenerationCoolDown;
         private readonly SpaceScavenger _myGame;
         private KeyboardState previousKbState;
 
@@ -19,23 +20,65 @@ namespace Space_Scavenger
         public Boost(Game game) : base(game)
         {
             _myGame = (SpaceScavenger) game;
+            NrOfBoosts = 3;
+            BoostTime = 200;
+            BoostRegenerationCoolDown = 0;
         }
-
+        //_myGame.Player.Speed =  new Vector2((float) Math.Cos(_myGame.Player.Rotation), (float) Math.Sin(_myGame.Player.Rotation)) * 20f;
         public override void Update(GameTime gameTime)
         {
             KeyboardState state = Keyboard.GetState();
-            if (state.IsKeyDown(Keys.X) && previousKbState.IsKeyDown(Keys.X) != state.IsKeyDown(Keys.X))
-            {
-                if (BoostTime <= 0)
-                {
-                    _myGame.Player.Speed = new Vector2((float) Math.Cos(_myGame.Player.Rotation), (float) Math.Sin(_myGame.Player.Rotation)) *
-                                   20f;
-                    BoostTime = 600;
-                }
 
-                if (BoostTime >= 0)
-                { BoostTime--;}
+            if (NrOfBoosts > 0)
+            {
+                if (BoostRegenerationCoolDown > 0)
+                {
+                    BoostRegenerationCoolDown--;
+                }
+                
+                if (state.IsKeyDown(Keys.X) && previousKbState.IsKeyDown(Keys.X) != state.IsKeyDown(Keys.X))
+                {
+                    _myGame.Player.Speed =
+                        new Vector2((float) Math.Cos(_myGame.Player.Rotation), (float) Math.Sin(_myGame.Player.Rotation)) *
+                        20f;
+                    NrOfBoosts--;
+                   BoostRegenerationCoolDown = 300;
+
+                }
+                if (BoostRegenerationCoolDown <= 0)
+                {
+                    if (NrOfBoosts >= 1 && NrOfBoosts < 3)
+                    {
+                        NrOfBoosts++;
+                        BoostRegenerationCoolDown = 300;
+                    }
+                }
+               
+                           
             }
+            else if (NrOfBoosts == 0)
+            {
+                if (BoostRegenerationCoolDown > 0)
+                {
+                    BoostRegenerationCoolDown--;
+                }
+                else if (BoostRegenerationCoolDown <= 0)
+                {
+                    NrOfBoosts++;
+                    BoostRegenerationCoolDown = 300;
+                }
+            }
+
+
+            // if (BoostRegenerationCoolDown > 0)
+            // {
+            //     BoostRegenerationCoolDown--;
+            // }
+            // if (BoostRegenerationCoolDown <= 0)
+            // {
+            //     NrOfBoosts++;
+            // }
+
 
             previousKbState = state;
         }
