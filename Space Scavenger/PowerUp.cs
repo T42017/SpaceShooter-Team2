c11a7062d2e85eb6a79cf94ec7a259c963e6b661
OskarMolander <oskar.molander@lund.ntig.se>
2017-09-14 14:09:58 +0200

    added red pill texture

---
+++ Space Scavenger/PowerUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Space_Scavenger
+{
+    class PowerUp : DrawableGameComponent
+    {
+        
+        public PowerUp(Game game) : base(game)
+        {
+
+        }
+    }
+}
